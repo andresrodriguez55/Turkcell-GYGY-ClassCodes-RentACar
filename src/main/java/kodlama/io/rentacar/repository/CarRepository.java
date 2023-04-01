99b39dd9012febda38206198d3db30921ecb0a4e
Andres Arturo Rodriguez Calderon <19671994kurtcobain19671994@gmail.com>
2023-04-01 13:12:33 +0300

    added anotations

--- src/main/java/kodlama/io/rentacar/repository/CarRepository.java
+++ src/main/java/kodlama/io/rentacar/repository/CarRepository.java
@@ -3,7 +3,9 @@ package kodlama.io.rentacar.repository;
 import kodlama.io.rentacar.entities.Brand;
 import kodlama.io.rentacar.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Integer>
 {
 }
